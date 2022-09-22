59f86a38c08f86501faad619a5c02b64b3f9b75b
sbx_user1051 <sbx_user1051@169.254.154.101>
2022-09-22 14:20:12 +0000

    Thu Sep 22 14:20:12 UTC 2022

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,13 +7,13 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
         String temp = new String();
         temp = number.toString();
-        if((number % 3 ==0 || temp.contains("3")) && (number % 5 ==0 || temp.contains("5"))&&(number>10 &&allDigitsSame(number)&&number%2!=0)) return  "fizz buzz fake deluxe";
-        else if ((number % 3 ==0 || temp.contains("3"))&&(number>10 &&allDigitsSame(number)&&number%2!=0)) return "fizz fake deluxe";
-        else if ((number % 5 ==0 || temp.contains("5"))&&(number>10 &&allDigitsSame(number)&&number%2!=0)) return "buzz fake deluxe";
-        else if ((number>10 &&allDigitsSame(number)&&number%2!=0)) return "fake deluxe";
-        else if((number % 3 ==0 || temp.contains("3")) && (number % 5 ==0 || temp.contains("5"))&&(number>10 &&allDigitsSame(number))) return  "fizz buzz deluxe";
-        else if ((number % 3 ==0 || temp.contains("3"))&&(number>10 &&allDigitsSame(number))) return "fizz deluxe";
-        else if ((number % 5 ==0 || temp.contains("5"))&&(number>10 &&allDigitsSame(number))) return "buzz deluxe";
+        if((number % 3 ==0 || temp.contains("3")) && (number % 5 ==0 || temp.contains("5"))&&(((number % 3 ==0 && temp.contains("3")) || (number % 5 ==0 && temp.contains("5")))&&number%2!=0)) return  "fizz buzz fake deluxe";
+        else if ((number % 3 ==0 || temp.contains("3"))&&(((number % 3 ==0 && temp.contains("3")) || (number % 5 ==0 && temp.contains("5")))&&number%2!=0)) return "fizz fake deluxe";
+        else if ((number % 5 ==0 || temp.contains("5"))&&(((number % 3 ==0 && temp.contains("3")) || (number % 5 ==0 && temp.contains("5")))&&number%2!=0)) return "buzz fake deluxe";
+        else if ((((number % 3 ==0 && temp.contains("3")) || (number % 5 ==0 && temp.contains("5")))&&number%2!=0)) return "fake deluxe";
+        else if((number % 3 ==0 || temp.contains("3")) && (number % 5 ==0 || temp.contains("5"))&&((number % 3 ==0 && temp.contains("3")) || (number % 5 ==0 && temp.contains("5")))) return  "fizz buzz deluxe";
+        else if ((number % 3 ==0 || temp.contains("3"))&&((number % 3 ==0 && temp.contains("3")) || (number % 5 ==0 && temp.contains("5")))) return "fizz deluxe";
+        else if ((number % 5 ==0 || temp.contains("5"))&&((number % 3 ==0 && temp.contains("3")) || (number % 5 ==0 && temp.contains("5")))) return "buzz deluxe";
         else if ((number>10 &&allDigitsSame(number))) return "deluxe";
         else if((number % 3 ==0 || temp.contains("3")) && (number % 5 ==0 || temp.contains("5"))) return  "fizz buzz";
         else if (number % 3 ==0 || temp.contains("3")) return "fizz";
@@ -29,3 +29,4 @@ public class FizzBuzzSolution {
         return true;
     }
 }
+
