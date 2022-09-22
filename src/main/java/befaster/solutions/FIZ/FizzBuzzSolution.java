1615f1ae026acc5c3d9701e0b96372408e30b52b
sbx_user1051 <sbx_user1051@169.254.154.101>
2022-09-22 14:01:30 +0000

    Thu Sep 22 14:01:30 UTC 2022

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,10 +7,22 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
         String temp = new String();
         temp = number.toString();
-        if((number % 3 ==0 || temp.contains("3")) && (number % 5 ==0 || temp.contains("5"))) return "fizz buzz";
+        if((number % 3 ==0 || temp.contains("3")) && (number % 5 ==0 || temp.contains("5"))) return  "fizz buzz";
         else if (number % 3 ==0 || temp.contains("3")) return "fizz";
-        else if (number % 5 ==0 || temp.contains("5") ) return "buzz";
-        else return number.toString();
+        else if (number % 5 ==0 || temp.contains("5") ) return  "buzz";
+        // deluxe
+        if((number % 3 ==0 || temp.contains("3")) && (number % 5 ==0 || temp.contains("5"))&&(number>10 &&allDigitsSame(number))) return  "fizz buzz deluxe";
+        else if ((number % 3 ==0 || temp.contains("3"))&&(number>10 &&allDigitsSame(number))) return "fizz deluxe";
+        else if ((number % 5 ==0 || temp.contains("5"))&&(number>10 &&allDigitsSame(number))) return "buzz deluxe";
+        else if ((number>10 &&allDigitsSame(number))) return "deluxe";
+        else return  number.toString();
     }
-
+    public boolean allDigitsSame(Integer number){
+        int t = number % 10;
+        while (number !=0){
+            if (number %10 !=t) return false;
+        }
+        return true;
+    }
 }
+
